496dbc0d9bf8917e75259a936044b0f8dcc08050
rothx00 <rothx00@gmail.com>
2024-01-03 16:34:35 +0100

    Init project adventure game

---
+++ adventure-game/adventure_game.py
@@ -0,0 +1,11 @@
+# Project Instructions
+# 1. Print descriptions of what's happening for the player
+# 2. Pausing between printing descriptions
+# 3. Give the player some choices
+# 4. Make sure the player gives a valid input
+# 5. Add functions and refactor your code
+# 6. Use randomness in your game
+# 7. Create win and lose conditions
+# 8. Check if the player wants to play again
+# 9. Check your style with pycodestyle
+# 10. Test Your Code
